57cb503df3584844543e017514182dd62caba6dd
ziyuan <hanziyuan08@outlook.com>
2018-08-30 18:02:09 +0800

    daka

--- .eslintrc.js
+++ .eslintrc.js
@@ -10,6 +10,9 @@ module.exports = {
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'indent': [0],
+    'linebreak-style': 0,
+    'semi': [0],
   },
   parserOptions: {
     parser: 'babel-eslint',
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,44 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div>
+      <span>名称: </span>
+      <el-input title="name" placeholder="请输入名称" v-model="name"></el-input>
+    </div>
+    <div>
+      <span>里程: </span>
+      <el-input title="distance" placeholder="请输入里程" v-model="distance"></el-input>
+    </div>
+    <el-button type="primary" @click="confirm">确认</el-button>
+
+    <div v-for="item in list" :key="item.name">
+      <span>{{item.name}}</span>
+      <span>{{item.distance}}</span>
+    </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue';
-
-export default {
-  name: 'home',
-  components: {
-    HelloWorld,
-  },
-};
+  export default {
+    name: 'home',
+    data() {
+      return {
+        name: '',
+        distance: '',
+        list: [],
+      }
+    },
+    methods: {
+      confirm() {
+        this.list.push({
+          name: this.name,
+          distance: this.distance,
+        })
+        this.clear()
+      },
+      clear() {
+        this.name = ''
+        this.distance = ''
+      },
+    },
+  };
 </script>
